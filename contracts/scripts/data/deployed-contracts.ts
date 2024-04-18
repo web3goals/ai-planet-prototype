9ad204c7b2351a9516b9eb82baf7d82d4b054203
kiv1n <aloha@kiv1n.ru>
2024-04-18 16:52:06 +0300

    Create and deploy contracts for AI app and USD token

---
+++ contracts/scripts/data/deployed-contracts.ts
@@ -0,0 +1,11 @@
+export const CONTRACTS: {
+  [key: string]: {
+    aiApp: `0x${string}` | undefined;
+    usdToken: `0x${string}` | undefined;
+  };
+} = {
+  arbitrumSepolia: {
+    aiApp: "0xe05B06f086BE56C22A4cc70708f95d28ca9A8320",
+    usdToken: "0x63a465e2C7Ca890430f81B7cE8Ad86bc9A4512DD",
+  },
+};

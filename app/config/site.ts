83ec73496714c122e5a0e933d1facc1581dc6523
kiv1n <aloha@kiv1n.ru>
2024-04-20 14:42:31 +0300

    Add support of account abstraction

--- app/config/site.ts
+++ app/config/site.ts
@@ -24,11 +24,11 @@ export const siteConfig = {
       chain: arbitrumSepolia,
       aiApp: "0xe05B06f086BE56C22A4cc70708f95d28ca9A8320" as `0x${string}`,
       usdt: "0x63a465e2C7Ca890430f81B7cE8Ad86bc9A4512DD" as `0x${string}`,
-      entryPoint: "0x0000000000000000000000000000000000000000" as `0x${string}`,
-      paymaster: "0x0000000000000000000000000000000000000000" as `0x${string}`,
+      entryPoint: "0xcee8564039B5620b847E91866e54d2DE3fCD10a0" as `0x${string}`,
+      paymaster: "0x0702Ae9aff3031a242233F01F1FB35b03AF18a50" as `0x${string}`,
       accountFactory:
-        "0x0000000000000000000000000000000000000000" as `0x${string}`,
-      accountAbstractionSuported: false,
+        "0xc254cDd94b834966DB91e99bb6aE073Df3F55Bd7" as `0x${string}`,
+      accountAbstractionSuported: true,
     } as SiteConfigContracts,
   },
   attestations: {
